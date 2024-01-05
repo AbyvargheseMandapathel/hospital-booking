2167e4c37a7359ecce00c17fef22e937b1436404
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2024-01-05 22:45:33 +0530

    booking

--- bvmhospital/forms.py
+++ bvmhospital/forms.py
@@ -1,18 +1,14 @@
+# forms.py
 from django import forms
-from .models import Booking
+from .models import Booking, Doctors
 
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 class BookingForm(forms.ModelForm):
-    DOCTOR_CHOICES = [
-        ("Dr. Arun", "Dr. Arun - General Medicine"),
-        ("Dr. Thomas", "Dr. Thomas - General Surgery"),
-        ("Dr. Anjali", "Dr. Anjali - Neurology"),
-        ("Dr. Sreelakshmi", "Dr. Sreelakshmi - Pediatrics"),
-    ]
+    doctor_choices = [(doctor.id, f"{doctor.doc_name} - {doctor.doc_spec}") for doctor in Doctors.objects.all()]
 
-    doc_name = forms.ChoiceField(choices=DOCTOR_CHOICES, label="Doctor")
+    doc_name = forms.ChoiceField(choices=doctor_choices, label="Doctor")
 
     class Meta:
         model = Booking
@@ -29,3 +25,7 @@ class BookingForm(forms.ModelForm):
             'booking_date': 'Booking Date',
             'doc_name': 'Doctor Name',
         }
+
+    def clean_doc_name(self):
+        doctor_id = self.cleaned_data['doc_name']
+        return Doctors.objects.get(id=doctor_id)
